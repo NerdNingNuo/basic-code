d963919a733e59b2653782c6bea8f05415ae462f
NingNuo <1776499617@qq.com>
2020-09-12 18:01:54 +0800

    di yi tian

---
+++ day04-code/src/cn/itcast/day04/demo01/HelloWorld.java
@@ -0,0 +1,13 @@
+package cn.itcast.day04.demo01;
+
+import org.w3c.dom.css.CSSStyleDeclaration;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+//        Scanner sc = null;
+//        System.out.println();
+    }
+}
